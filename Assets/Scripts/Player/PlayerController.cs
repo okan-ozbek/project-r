e1d137cb1f24a3550faee019166249616396c232
Okan Özbek <o.ozbek@tjip.com>
2024-01-25 16:57:50 +0100

    Start player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,18 +2,45 @@ using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        public float accelerationSpeed;
+        public float decelerationSpeed;
+        public float maxAccelerationSpeed;
         
+        private Vector3 _velocity;
+        
+        private Rigidbody _rigidbody;
+        
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
         }
-
-        // Update is called once per frame
-        void Update()
-        {
         
+        private void Update()
+        {
+            Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+            input.Normalize();
+
+            if (input != Vector3.zero)
+            {
+                _velocity += new Vector3(
+                    Mathf.Min(input.x * accelerationSpeed * Time.deltaTime, maxAccelerationSpeed), 
+                    0.0f,
+                    Mathf.Min(input.z * accelerationSpeed * Time.deltaTime, maxAccelerationSpeed)
+                );
+            }
+            else
+            {
+                _velocity -= new Vector3(
+                    Mathf.Max(_velocity.x - decelerationSpeed * Time.deltaTime, 0.0f),
+                    0.0f,
+                    Mathf.Max(_velocity.z - decelerationSpeed * Time.deltaTime, 0.0f)
+                );
+            }
+
+            _rigidbody.velocity = _velocity;
         }
     }
 }
