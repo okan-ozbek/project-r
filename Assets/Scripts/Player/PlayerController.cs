e8c46e745fc54eb895ba8c24ca89380f363fc5f8
Okan Ozbek <devokan@outlook.com>
2024-01-25 21:48:18 +0100

    Temporary movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,16 +6,11 @@ namespace Player
     public class PlayerController : MonoBehaviour
     {
         public float accelerationSpeed;
-        public float decelerationSpeed;
-        public float maxAccelerationSpeed;
-        
-        private Vector3 _velocity;
-        
+
         private Rigidbody _rigidbody;
-        
+
         private void Start()
         {
-            _rigidbody = GetComponent<Rigidbody>();
         }
         
         private void Update()
@@ -23,24 +18,7 @@ namespace Player
             Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
             input.Normalize();
 
-            if (input != Vector3.zero)
-            {
-                _velocity += new Vector3(
-                    Mathf.Min(input.x * accelerationSpeed * Time.deltaTime, maxAccelerationSpeed), 
-                    0.0f,
-                    Mathf.Min(input.z * accelerationSpeed * Time.deltaTime, maxAccelerationSpeed)
-                );
-            }
-            else
-            {
-                _velocity -= new Vector3(
-                    Mathf.Max(_velocity.x - decelerationSpeed * Time.deltaTime, 0.0f),
-                    0.0f,
-                    Mathf.Max(_velocity.z - decelerationSpeed * Time.deltaTime, 0.0f)
-                );
-            }
-
-            _rigidbody.velocity = _velocity;
+            transform.Translate(input * accelerationSpeed * Time.deltaTime);
         }
     }
 }
