d7ff140a3d44f5025dc0f727da144dedd89a2b5e
Okan Ozbek <devokan@outlook.com>
2024-02-13 00:17:52 +0100

    Auto stash before rebase of "feature/player-controller"

---
+++ Assets/Tests/Edit/EditModeTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace Tests.Edit
+{
+    public class EditModeTests
+    {
+        [Test]
+        public void CheckSumPasses()
+        {
+            const int a = 2;
+            const int b = 3;
+
+            Assert.IsNotNull(a);
+            Assert.IsNotNull(b);
+            Assert.AreEqual((a + b), 5);
+            Assert.AreNotEqual(a, b);
+        }
+
+        [Test]
+        public void CheckStringPasses()
+        {
+            const string a = "Peepeepoopoo";
+
+            Assert.IsNotNull(a);
+            Assert.AreEqual("Peepeepoopoo", a);
+        }
+    }
+}
